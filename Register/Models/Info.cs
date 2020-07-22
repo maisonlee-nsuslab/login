b2f40a0d44c8a4524279f845c351c8e946060d4a
maisonlee-nsuslab <maison.lee@nsuslab.com>
2020-07-22 15:16:30 +0900

    login

---
+++ Register/Models/Info.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Register.Models
+{
+    public class Info
+    {
+        [Required]
+        [BindProperty]
+        public int Id { get; set; }
+        [StringLength(10, MinimumLength = 1)]
+        public string userId { get; set; }
+
+        
+        public string userName { get; set; }
+      
+        [StringLength(8, MinimumLength = 1)]
+        public string userPassword { get; set; }
+        
+        public bool login { get; set; }
+
+        [Compare("userPassword")]
+        public string ConfirmPassword { get; set; }
+    }
+}
