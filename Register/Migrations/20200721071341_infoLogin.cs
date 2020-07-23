8554006dc45e35bac4bdd4a91be1818efd9803f2
maisonlee-nsuslab <maison.lee@nsuslab.com>
2020-07-23 16:24:28 +0900

    jaeyo

---
+++ Register/Migrations/20200721071341_infoLogin.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Register.Migrations
+{
+    public partial class infoLogin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "checkPassword",
+                table: "Info");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "login",
+                table: "Info",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "login",
+                table: "Info");
+
+            migrationBuilder.AddColumn<string>(
+                name: "checkPassword",
+                table: "Info",
+                type: "nvarchar(8)",
+                maxLength: 8,
+                nullable: true);
+        }
+    }
+}
