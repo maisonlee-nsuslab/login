a61b154dfb58fb003f2d3af1ddb48d12aa5cac10
maisonlee-nsuslab <maison.lee@nsuslab.com>
2020-07-23 16:26:18 +0900

    2nd

---
+++ RegisterTests/Controllers/LoginControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Register.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Register.Controllers.Tests
+{
+    [TestClass()]
+    public class LoginControllerTests
+    {
+        [TestMethod()]
+        public void LoginControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoginTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RegisterTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RegisterTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ComputeSha256HashTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
